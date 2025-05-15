a43ec0a0ecb7a2bd6eb907a039fb464af899db66
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-15 21:57:00 +0200

    add Cmd Invoker Echo test

--- tests/CliInvoke.Specializations.Tests/Invokers/CmdInvokerTests.cs
+++ tests/CliInvoke.Specializations.Tests/Invokers/CmdInvokerTests.cs
@@ -1,21 +1,54 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿
+using System.Threading;
+using System.Threading.Tasks;
+using AlastairLundy.CliInvoke;
+using AlastairLundy.CliInvoke.Abstractions;
+using AlastairLundy.CliInvoke.Builders;
+using AlastairLundy.CliInvoke.Builders.Abstractions;
+using AlastairLundy.CliInvoke.Extensibility.Abstractions.Invokers;
+using AlastairLundy.CliInvoke.Specializations.Invokers;
+using AlastairLundy.Extensions.Processes.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CliInvoke.Specializations.Tests.Invokers;
 
-#if NET472_OR_GREATER
-using OperatingSystem = OperatingSystemPolyfill;
+#if NET48_OR_GREATER
+using OperatingSystem = Polyfills.OperatingSystemPolyfill;
 #endif
 
-public class CmdInvokerTests
+public class CmdInvokerTests : IClassFixture<TestFixture>
 {
+    private readonly ISpecializedCliCommandInvoker _specializedCliCommandInvoker;
+    
+    public CmdInvokerTests(TestFixture testFixture)
+    {
+        ICliCommandInvoker cliInvoker = testFixture.ServiceProvider.GetRequiredService<ICliCommandInvoker>();
+        
+        _specializedCliCommandInvoker = new CmdCliCommandInvoker(cliInvoker);
+    }
     
     [Fact]
-    public void Invoke_Echo()
+    public async Task Invoke_Echo_With_CMD_Test()
     {
+#if NET48_OR_GREATER || NET5_0_OR_GREATER
         if (OperatingSystem.IsWindows())
+#else
+        if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+#endif
         {
-            RuntimeInformation.IsOSPlatform()
+            ICliCommandConfigurationBuilder configurationBuilder = new CliCommandConfigurationBuilder("echo")
+                .WithArguments("test")
+                .WithWorkingDirectory(Environment.SystemDirectory)
+                .WithValidation(ProcessResultValidation.ExitCodeZero);
+            
+            CliCommandConfiguration commandConfiguration = configurationBuilder.Build();
+
+            CliCommandConfiguration runnerCommand = _specializedCliCommandInvoker.CreateRunnerCommand(commandConfiguration);
+
+            BufferedProcessResult result = await _specializedCliCommandInvoker.ExecuteBufferedAsync(runnerCommand, CancellationToken.None);
+            
+            Assert.Equal("test", result.StandardOutput);
         }
+        
     }
 }
