da29948724e366f43a6cf3172e38129d49360fac
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-17 23:18:38 +0100

    Create ExecutedCommandHelper.cs

---
+++ tests/CliInvoke.Specializations.Tests/Helpers/ExecutedCommandHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CliInvoke.Specializations.Tests.Helpers
+{
+    public static class ExecutedCommandHelper
+    {
+        public static string WinCalcExePath
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return Environment.SystemDirectory + Path.DirectorySeparatorChar
+                                                       + "System32" + Path.DirectorySeparatorChar
+                                                       + "calc.exe";
+                }
+                
+                throw new PlatformNotSupportedException();
+            }
+        }
+    }
+}
