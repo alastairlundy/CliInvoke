e7974e026e81236916f4bdda080faee429302286
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-25 19:29:45 +0100

    cleanup benchmarks program

--- src/CliInvoke.Benchmarks/Program.cs
+++ src/CliInvoke.Benchmarks/Program.cs
@@ -1,16 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Reflection;
-using AlastairLundy.CliInvoke.Abstractions;
-using AlastairLundy.CliInvoke.Core.Abstractions;
-using AlastairLundy.CliInvoke.Extensions;
 
 using BenchmarkDotNet.Running;
 
 using CliInvoke.Benchmarking.Benchmarks.Invokation;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 
-
-BenchmarkRunner.Run(Assembly.GetExecutingAssembly());
+BenchmarkRunner.Run<DotnetUnbufferedInvokationBenchmark>();
+//BenchmarkRunner.Run(Assembly.GetExecutingAssembly());
