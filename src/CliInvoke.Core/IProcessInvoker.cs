a1b1caba2e1f9f07971d4b2aed803743112671fe
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-23 21:16:29 +0100

    Update IProcessInvoker.cs

--- src/CliInvoke.Core/IProcessInvoker.cs
+++ src/CliInvoke.Core/IProcessInvoker.cs
@@ -16,89 +16,88 @@ using AlastairLundy.CliInvoke.Core.Primitives;
 using AlastairLundy.CliInvoke.Core.Primitives.Policies;
 using AlastairLundy.CliInvoke.Core.Primitives.Results;
 
-namespace AlastairLundy.CliInvoke.Core
+namespace AlastairLundy.CliInvoke.Core;
+
+/// <summary>
+/// Defines the contract for a class that executes processes.
+/// </summary>
+public interface IProcessInvoker
 {
     /// <summary>
-    /// Defines the contract for a class that executes processes.
+    /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
     /// </summary>
-    public interface IProcessInvoker
-    {
-        /// <summary>
-        /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
-        /// </summary>
-        /// <param name="processConfiguration"></param>
-        /// <param name="cancellationToken">A token to cancel the operation if required.</param>
-        /// <returns>The Process Results from the running the process.</returns>
-        Task<ProcessResult> ExecuteAsync(ProcessConfiguration processConfiguration,
-            CancellationToken cancellationToken = default);
+    /// <param name="processConfiguration"></param>
+    /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+    /// <returns>The Process Results from the running the process.</returns>
+    Task<ProcessResult> ExecuteAsync(ProcessConfiguration processConfiguration,
+        CancellationToken cancellationToken = default);
 
-        /// <summary>
-        /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
-        /// </summary>
-        /// <param name="processStartInfo"></param>
-        /// <param name="processResultValidation">The process result validation to be used.</param>
-        /// <param name="processResourcePolicy">The process resource policy to be set if it is not null.</param>
-        /// <param name="userCredential"></param>
-        /// <param name="standardInput"></param>
-        /// <param name="cancellationToken">A token to cancel the operation if required.</param>
-        /// <returns>The Process Results from the running the process.</returns>
-        Task<ProcessResult> ExecuteAsync(ProcessStartInfo processStartInfo,
-             ProcessResultValidation processResultValidation,
-             ProcessResourcePolicy? processResourcePolicy = null,
-             UserCredential? userCredential = null,
-             StreamWriter? standardInput = null,
-             CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <param name="processStartInfo"></param>
+    /// <param name="processResultValidation">The process result validation to be used.</param>
+    /// <param name="processResourcePolicy">The process resource policy to be set if it is not null.</param>
+    /// <param name="userCredential"></param>
+    /// <param name="standardInput"></param>
+    /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+    /// <returns>The Process Results from the running the process.</returns>
+    Task<ProcessResult> ExecuteAsync(ProcessStartInfo processStartInfo,
+        ProcessResultValidation processResultValidation,
+        ProcessResourcePolicy? processResourcePolicy = null,
+        UserCredential? userCredential = null,
+        StreamWriter? standardInput = null,
+        CancellationToken cancellationToken = default);
 
-        /// <summary>
-        /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
-        /// </summary>
-        /// <param name="processConfiguration"></param>
-        /// <param name="cancellationToken">A token to cancel the operation if required.</param>
-        /// <returns>The Buffered Process Results from running the process.</returns>
-         Task<BufferedProcessResult> ExecuteBufferedAsync(ProcessConfiguration processConfiguration,
-            CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <param name="processConfiguration"></param>
+    /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+    /// <returns>The Buffered Process Results from running the process.</returns>
+    Task<BufferedProcessResult> ExecuteBufferedAsync(ProcessConfiguration processConfiguration,
+        CancellationToken cancellationToken = default);
 
-        /// <summary>
-        /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
-        /// </summary>
-        /// <param name="processStartInfo"></param>
-        /// <param name="processResultValidation">The process result validation to be used.</param>
-        /// <param name="processResourcePolicy">The process resource policy to be set if it is not null.</param>
-        /// <param name="userCredential"></param>
-        /// <param name="standardInput"></param>
-        /// <param name="cancellationToken">A token to cancel the operation if required.</param>
-        /// <returns>The Buffered Process Results from running the process.</returns>
-        Task<BufferedProcessResult> ExecuteBufferedAsync(ProcessStartInfo processStartInfo,
-            ProcessResultValidation processResultValidation,
-            ProcessResourcePolicy? processResourcePolicy = null,
-            UserCredential? userCredential = null,
-            StreamWriter? standardInput = null,
-            CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <param name="processStartInfo"></param>
+    /// <param name="processResultValidation">The process result validation to be used.</param>
+    /// <param name="processResourcePolicy">The process resource policy to be set if it is not null.</param>
+    /// <param name="userCredential"></param>
+    /// <param name="standardInput"></param>
+    /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+    /// <returns>The Buffered Process Results from running the process.</returns>
+    Task<BufferedProcessResult> ExecuteBufferedAsync(ProcessStartInfo processStartInfo,
+        ProcessResultValidation processResultValidation,
+        ProcessResourcePolicy? processResourcePolicy = null,
+        UserCredential? userCredential = null,
+        StreamWriter? standardInput = null,
+        CancellationToken cancellationToken = default);
         
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="processConfiguration"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        Task<PipedProcessResult> ExecutePipedAsync(ProcessConfiguration processConfiguration,
-            CancellationToken cancellationToken = default);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="processConfiguration"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<PipedProcessResult> ExecutePipedAsync(ProcessConfiguration processConfiguration,
+        CancellationToken cancellationToken = default);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="processStartInfo"></param>
-        /// <param name="processResultValidation"></param>
-        /// <param name="processResourcePolicy"></param>
-        /// <param name="userCredential"></param>
-        /// <param name="standardInput"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        Task<PipedProcessResult> ExecutePipedAsync(ProcessStartInfo processStartInfo,
-            ProcessResultValidation processResultValidation,
-            ProcessResourcePolicy? processResourcePolicy = null,
-            UserCredential? userCredential = null,
-            StreamWriter? standardInput = null,
-            CancellationToken cancellationToken = default);
-    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="processStartInfo"></param>
+    /// <param name="processResultValidation"></param>
+    /// <param name="processResourcePolicy"></param>
+    /// <param name="userCredential"></param>
+    /// <param name="standardInput"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<PipedProcessResult> ExecutePipedAsync(ProcessStartInfo processStartInfo,
+        ProcessResultValidation processResultValidation,
+        ProcessResourcePolicy? processResourcePolicy = null,
+        UserCredential? userCredential = null,
+        StreamWriter? standardInput = null,
+        CancellationToken cancellationToken = default);
 }
