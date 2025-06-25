0cb33e1cdbdc8833d728cd55ccb35ea6dc4b8870
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-25 14:42:50 +0100

    fix incorrect namespace

--- src/CliInvoke.Core/Builders/ProcessResourcePolicyBuilder.cs
+++ src/CliInvoke.Core/Builders/ProcessResourcePolicyBuilder.cs
@@ -11,9 +11,10 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
-using AlastairLundy.CliInvoke.Core.Abstractions.Builders;
+
 using AlastairLundy.CliInvoke.Core.Builders.Abstractions;
 using AlastairLundy.CliInvoke.Core.Primitives.Policies;
+    
 #if NET5_0_OR_GREATER
 using System.Runtime.Versioning;
 #endif
