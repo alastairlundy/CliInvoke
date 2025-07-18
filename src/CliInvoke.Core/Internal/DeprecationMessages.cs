76fe9486a81feae1ac6c28f864fa0eb89f407c73
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-07-18 10:09:37 +0100

    remove deprecation messages

--- src/CliInvoke.Core/Internal/DeprecationMessages.cs
+++ src/CliInvoke.Core/Internal/DeprecationMessages.cs
@@ -2,12 +2,5 @@
 
 internal static class DeprecationMessages
 {
-    internal const string ClassDeprecationV3 = "This class is deprecated and will be removed in version 2.0";
-
-    //internal const string ClassDeprecationV2UseProcessFactoryInstead = "This class is deprecated and will be removed in version 2.0. Use ProcessFactory instead.";
-
-    //internal const string InterfaceDeprecationV2UseProcessFactoryInstead = "This class is deprecated and will be removed in version 2.0. Use IProcessFactory instead.";
-
-    
-    internal const string InterfaceDeprecationV3 = "This interface is deprecated and will be removed in version 2.0";
+   
 }
