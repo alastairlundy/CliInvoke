896aa2e3c3bed0d9a36f5ffe93df4b65c5778c85
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-07 23:27:53 +0200

    implement ProcessExitInfoBuilder

--- src/CliInvoke/Builders/ProcessExitInfoBuilder.cs
+++ src/CliInvoke/Builders/ProcessExitInfoBuilder.cs
@@ -3,20 +3,48 @@ using AlastairLundy.CliInvoke.Core.Primitives;
 
 namespace AlastairLundy.CliInvoke.Builders;
 
+/// <summary>
+/// 
+/// </summary>
 public class ProcessExitInfoBuilder : IProcessExitInfoBuilder
 {
+    private readonly ProcessExitInfo _processExitInfo;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="processExitInfo"></param>
+    public ProcessExitInfoBuilder(ProcessExitInfo processExitInfo)
+    {
+        _processExitInfo = processExitInfo;
+    }
+        
+    /// <summary>
+    /// Sets the Result Validation whether to throw an exception or not if the Process does not execute successfully.
+    /// </summary>
+    /// <param name="validation">The result validation behaviour to be used.</param>
+    /// <returns>The new ProcessExitInfoBuilder object with the configured Result Validation behaviour.</returns>
     public IProcessExitInfoBuilder WithValidation(ProcessResultValidation validation)
     {
-        throw new System.NotImplementedException();
+        return new ProcessExitInfoBuilder(
+            new ProcessExitInfo(_processExitInfo.TimeoutPolicy, validation));
     }
 
+    /// <summary>
+    /// Sets the Process Timeout Policy to be used for this Process.
+    /// </summary>
+    /// <param name="processTimeoutPolicy">The process timeout policy to use.</param>
+    /// <returns>The new ProcessExitInfoBuilder with the specified Process Timeout Policy.</returns>
     public IProcessExitInfoBuilder WithProcessTimeoutPolicy(ProcessTimeoutPolicy processTimeoutPolicy)
     {
-        throw new System.NotImplementedException();
+        return new ProcessExitInfoBuilder(
+            new ProcessExitInfo(processTimeoutPolicy, _processExitInfo.ResultValidation));
     }
 
-    public ProcessExitInfo Build()
-    {
-        throw new System.NotImplementedException();
-    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public ProcessExitInfo Build() =>  
+        _processExitInfo;
 }
