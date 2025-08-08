4d6d31d4b1ceb2e719e07cb3c3c0eccab494a4c2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-08 18:41:00 +0200

    Update ProcessExitInfoBuilder.cs

--- src/CliInvoke/Builders/ProcessExitInfoBuilder.cs
+++ src/CliInvoke/Builders/ProcessExitInfoBuilder.cs
@@ -1,4 +1,15 @@
+/*
+    AlastairLundy.CliInvoke
+     
+    Copyright (C) 2024-2025  Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 using AlastairLundy.CliInvoke.Core.Builders;
+
 using AlastairLundy.CliInvoke.Core.Primitives;
 
 namespace AlastairLundy.CliInvoke.Builders;
@@ -24,22 +35,18 @@ public class ProcessExitInfoBuilder : IProcessExitInfoBuilder
     /// </summary>
     /// <param name="validation">The result validation behaviour to be used.</param>
     /// <returns>The new ProcessExitInfoBuilder object with the configured Result Validation behaviour.</returns>
-    public IProcessExitInfoBuilder WithValidation(ProcessResultValidation validation)
-    {
-        return new ProcessExitInfoBuilder(
+    public IProcessExitInfoBuilder WithValidation(ProcessResultValidation validation) =>
+        new ProcessExitInfoBuilder(
             new ProcessExitInfo(_processExitInfo.TimeoutPolicy, validation));
-    }
 
     /// <summary>
     /// Sets the Process Timeout Policy to be used for this Process.
     /// </summary>
     /// <param name="processTimeoutPolicy">The process timeout policy to use.</param>
     /// <returns>The new ProcessExitInfoBuilder with the specified Process Timeout Policy.</returns>
-    public IProcessExitInfoBuilder WithProcessTimeoutPolicy(ProcessTimeoutPolicy processTimeoutPolicy)
-    {
-        return new ProcessExitInfoBuilder(
+    public IProcessExitInfoBuilder WithProcessTimeoutPolicy(ProcessTimeoutPolicy processTimeoutPolicy) =>
+        new ProcessExitInfoBuilder(
             new ProcessExitInfo(processTimeoutPolicy, _processExitInfo.ResultValidation));
-    }
 
     /// <summary>
     /// 
