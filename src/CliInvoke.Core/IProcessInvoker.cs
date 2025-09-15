d34b86043f1ea1917b391c9001f939abe6344564
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-15 19:36:24 +0100

    modify iprocessinvoker to add overloads that accept proces start info with fewer parameters

--- src/CliInvoke.Core/IProcessInvoker.cs
+++ src/CliInvoke.Core/IProcessInvoker.cs
@@ -37,6 +37,19 @@ public interface IProcessInvoker
     /// </summary>
     /// <param name="processStartInfo">The process start information to use.</param>
     /// <param name="processExitInfo">The process exit information to use.</param>
+    /// <param name="standardInput">The Stream to redirect to the Standard Input if not null.</param>
+    /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+    /// <returns>The Process Results from the running the process.</returns>
+    Task<ProcessResult> ExecuteAsync(ProcessStartInfo processStartInfo,
+        ProcessExitInfo? processExitInfo = null,
+        StreamWriter? standardInput = null,
+        CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <param name="processStartInfo">The process start information to use.</param>
+    /// <param name="processExitInfo">The process exit information to use.</param>
     /// <param name="processResourcePolicy">The process resource policy to be set if it is not null.</param>
     /// <param name="userCredential">The credential to use when creating and starting the Process.</param>
     /// <param name="standardInput">The Stream to redirect to the Standard Input if not null.</param>
@@ -65,6 +78,19 @@ public interface IProcessInvoker
     /// </summary>
     /// <param name="processStartInfo">The process start information to use.</param>
     /// <param name="processExitInfo">The process exit information to use.</param>
+    /// <param name="standardInput">The Stream to redirect to the Standard Input if not null.</param>
+    /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+    /// <returns>The Buffered Process Result that is returned from running the process.</returns>
+    Task<BufferedProcessResult> ExecuteBufferedAsync(ProcessStartInfo processStartInfo,
+        ProcessExitInfo? processExitInfo = null,
+        StreamWriter? standardInput = null,
+        CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Runs the process asynchronously, waits for exit, reads the standard output, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <param name="processStartInfo">The process start information to use.</param>
+    /// <param name="processExitInfo">The process exit information to use.</param>
     /// <param name="processResourcePolicy">The process resource policy to be set if it is not null.</param>
     /// <param name="userCredential">The credential to use when creating and starting the Process.</param>
     /// <param name="standardInput">The Stream to redirect to the Standard Input if not null.</param>
@@ -87,6 +113,20 @@ public interface IProcessInvoker
     /// <returns>The Piped Process Result that is returned from running the process.</returns>
     Task<PipedProcessResult> ExecutePipedAsync(ProcessConfiguration processConfiguration,
         ProcessExitInfo? processExitInfo = null,
+        CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Pipes the Standard Input, runs the process asynchronously,
+    /// waits for exit, pipes the standard output and error, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <param name="processStartInfo">The process start information to use.</param>
+    /// <param name="processExitInfo">The process exit information to use.</param>
+    /// <param name="standardInput">The Stream to redirect to the Standard Input if not null.</param>
+    /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+    /// <returns>The Piped Process Result that is returned from running the process.</returns>
+    Task<PipedProcessResult> ExecutePipedAsync(ProcessStartInfo processStartInfo,
+        ProcessExitInfo? processExitInfo = null,
+        StreamWriter? standardInput = null,
         CancellationToken cancellationToken = default);
 
     /// <summary>
