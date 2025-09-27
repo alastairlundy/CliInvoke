766bc97319fd33c78f0f7cb7923402318e3627a0
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-27 22:44:25 +0100

    Update ProcessApplyExtensions.cs

--- src/CliInvoke.Core/Extensions/Processes/ProcessApplyExtensions.cs
+++ src/CliInvoke.Core/Extensions/Processes/ProcessApplyExtensions.cs
@@ -27,35 +27,6 @@ namespace AlastairLundy.CliInvoke.Core.Extensions.Processes;
 
 public static class ProcessApplyExtensions
 {
-    /// <summary>
-    /// Attempts to add the specified Credential to the current Process object.
-    /// </summary>
-    /// <param name="process">The current Process object.</param>
-    /// <param name="credential">The credential to be added.</param>
-    /// <returns>True if successfully applied; false otherwise.</returns>
-    [Obsolete(DeprecationMessages.ClassDeprecationV2)]
-    public static bool TryApplyUserCredential(this Process process, UserCredential credential)
-    {
-        if (OperatingSystem.IsWindows())
-        {
-            try
-            {
-#pragma warning disable CA1416
-                ApplyUserCredential(process, credential);
-#pragma warning restore CA1416
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-        else
-        {
-            return false;
-        }
-    }
-
 
     /// <summary>
     /// Adds the specified Credential to the current Process object.
