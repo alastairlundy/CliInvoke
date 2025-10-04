0ef3cf17944b2aa6028569f368409fac5c9b80a4
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-04 18:40:07 +0100

    move files

--- tests/CliInvoke.Tests/Internal/Helpers/WindowsTestExecutables.cs
+++ tests/CliInvoke.Tests/Internal/Helpers/WindowsTestExecutables.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Runtime.InteropServices;
 
-namespace AlastairLundy.CliInvoke.Tests.Helpers;
+namespace AlastairLundy.CliInvoke.Tests.Internal.Helpers;
 
 public static class WindowsTestExecutables
 {
