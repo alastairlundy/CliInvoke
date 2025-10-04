012427ebd0f28d48b349e16a19fff0991c0ab112
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-04 18:40:44 +0100

    clean up test classes

--- tests/CliInvoke.Tests/Invokers/ProcessInvokerTests.cs
+++ tests/CliInvoke.Tests/Invokers/ProcessInvokerTests.cs
@@ -1,15 +1,8 @@
 using Xunit;
 
-namespace AlastairLundy.CliInvoke.Tests;
+namespace AlastairLundy.CliInvoke.Tests.Invokers;
 
 public class ProcessInvokerTests
 {
-    [Fact]
-    public void Constructor_ShouldInstantiate()
-    {
-        // Use mocks for dependencies if needed
-        var invoker = new ProcessConfigurationInvoker(null, null);
-        Assert.NotNull(invoker);
-    }
-    // Add more tests for Invoke, RunAsync, etc.
+    
 }
