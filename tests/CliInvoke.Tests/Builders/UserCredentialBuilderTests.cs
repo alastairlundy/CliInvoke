7bb9826426de177e8b7753a958dfb4f7285399c7
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-05 15:36:40 +0100

    Update UserCredentialBuilderTests.cs

--- tests/CliInvoke.Tests/Builders/UserCredentialBuilderTests.cs
+++ tests/CliInvoke.Tests/Builders/UserCredentialBuilderTests.cs
@@ -1,13 +1,19 @@
-﻿using System.Security;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security;
 using AlastairLundy.CliInvoke.Builders;
 using AlastairLundy.CliInvoke.Core.Builders;
 using AlastairLundy.CliInvoke.Core.Primitives;
+using Bogus;
 using Xunit;
+// ReSharper disable JoinDeclarationAndInitializer
 
 namespace AlastairLundy.CliInvoke.Tests.Builders;
 
+[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
 public class UserCredentialBuilderTests
 {
+    private readonly Faker _faker = new Faker();
+    
     [Fact]
     public void Constructor_ShouldInstantiate()
     {
@@ -21,7 +27,7 @@ public class UserCredentialBuilderTests
         // Arrange
         // ReSharper disable once JoinDeclarationAndInitializer
         IUserCredentialBuilder builder;
-       string userName = "fakeusername";
+        string userName = _faker.Internet.UserName();
        
        // Act
        builder = new UserCredentialBuilder()
@@ -57,10 +63,93 @@ public class UserCredentialBuilderTests
         Assert.Equal(credential.Password, password);
 #pragma warning restore CA1416
     }
+    
+    [Fact]
+    public void WithDomain_ShouldSetDomain()
+    {
+        //Arrange
+        string domain = _faker.Internet.DomainName();
+
+
+        //Act
+        IUserCredentialBuilder builder = new UserCredentialBuilder()
+            .SetDomain(domain);
+
+        UserCredential credential = builder.Build();
+        
+        //Assert
+#pragma warning disable CA1416
+        Assert.Equal(expected: credential.Domain, actual: domain);
+#pragma warning restore CA1416
+    }
 
     [Fact]
-    public void Build_ShouldReturnCredential()
+    public void LoadUserProfile_True_ShouldSetUserProfile()
     {
-        // TODO: Test Build method
+        //Arrange
+        bool loadUserProfile = true;
+        
+        //Arrange
+        IUserCredentialBuilder builder = new UserCredentialBuilder()
+            .LoadUserProfile(loadUserProfile);
+        
+        UserCredential credential = builder.Build();
+        
+        //Assert
+        Assert.True(credential.LoadUserProfile);
+    }
+    
+    [Fact]
+    public void LoadUserProfile_False_ShouldNotSetUserProfile()
+    {
+        //Arrange
+        bool loadUserProfile = false;
+        
+        //Arrange
+        IUserCredentialBuilder builder = new UserCredentialBuilder()
+            .LoadUserProfile(loadUserProfile);
+        
+        UserCredential credential = builder.Build();
+        
+        //Assert
+        Assert.False(credential.LoadUserProfile);
+    }
+    
+    [Fact]
+    public void Build_All_ShouldReturnCredential()
+    {
+        //Arrange
+        IUserCredentialBuilder builder;
+        
+        SecureString password = new SecureString();
+        password.AppendChar('f');
+        password.AppendChar('a');
+        password.AppendChar('k');
+        password.AppendChar('e');
+       
+        string domain =  _faker.Internet.DomainName();
+        string userName = _faker.Internet.UserName();
+        bool loadUserProfile = true;
+        
+        //Act
+        builder = new UserCredentialBuilder()
+            .SetDomain(domain)
+            .SetUsername(userName)
+            .SetPassword(password)
+            .LoadUserProfile(loadUserProfile);
+        
+        UserCredential credential = builder.Build();
+        
+        //Assert
+        Assert.NotNull(credential);
+        Assert.NotNull(credential.Domain);
+        Assert.NotNull(credential.Password);
+        Assert.NotNull(credential.UserName);
+        Assert.NotNull(credential.LoadUserProfile);
+        
+        Assert.Equal(credential.Domain, domain);
+        Assert.Equal(credential.UserName, userName);
+        Assert.Equal(credential.Password, password);
+        Assert.Equal(credential.LoadUserProfile, loadUserProfile);
     }
 }
