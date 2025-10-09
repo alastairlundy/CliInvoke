e4b863e1dc2b706cfb15ba01e0d4e133c9c3650b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-09 19:13:33 +0100

    Update ApplyConfigurationToProcessStartInfo.cs

--- src/CliInvoke/Helpers/Processes/ApplyInfos/ApplyConfigurationToProcessStartInfo.cs
+++ src/CliInvoke/Helpers/Processes/ApplyInfos/ApplyConfigurationToProcessStartInfo.cs
@@ -10,6 +10,7 @@
 
 using System.Diagnostics;
 using System.Runtime.Versioning;
+using AlastairLundy.CliInvoke.Core;
 
 #if NETSTANDARD2_0
 using OperatingSystem = Polyfills.OperatingSystemPolyfill;
