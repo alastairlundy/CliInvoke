8d23546206d3da0b1d30af90f675e9eaff54b1c3
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-09 19:48:48 +0100

    Update EnvironmentVariablesBuilder.cs

--- src/CliInvoke/Builders/EnvironmentVariablesBuilder.cs
+++ src/CliInvoke/Builders/EnvironmentVariablesBuilder.cs
@@ -58,25 +58,16 @@ public class EnvironmentVariablesBuilder : IEnvironmentVariablesBuilder
     /// <param name="value">The value of the environment variable to set.</param>
     /// <returns>A new instance of the IEnvironmentVariablesBuilder with the updated environment variables.</returns>
     [Pure]
-    public IEnvironmentVariablesBuilder Set(string name, string value){
+    public IEnvironmentVariablesBuilder Set(string name, string value)
+    {
         Dictionary<string, string> output = new Dictionary<string, string>(_environmentVariables,
             StringComparer.Ordinal) { { name, value } };
 
         return new EnvironmentVariablesBuilder(output);
     }
 
-    /// <summary>
-    /// Sets multiple environment variables.
-    /// </summary>
-    /// <param name="variables">The environment variables to set.</param>
-    /// <returns>A new instance of the IEnvironmentVariablesBuilder with the updated environment variables.</returns>
-    [Pure]
-    public IEnvironmentVariablesBuilder Set(IEnumerable<KeyValuePair<string, string>> variables)
     protected IEnvironmentVariablesBuilder SetInternal(IEnumerable<KeyValuePair<string, string>> variables)
     {
-        #if NET8_0_OR_GREATER
-        ArgumentNullException.ThrowIfNull(nameof(variables));
-        #endif
 #if NET8_0_OR_GREATER
         ArgumentNullException.ThrowIfNull(variables, nameof(variables));
 #endif
@@ -87,6 +78,15 @@ public class EnvironmentVariablesBuilder : IEnvironmentVariablesBuilder
         
         return new EnvironmentVariablesBuilder(output);
     }
+    
+    /// <summary>
+    /// Sets multiple environment variables.
+    /// </summary>
+    /// <param name="variables">The environment variables to set.</param>
+    /// <returns>A new instance of the IEnvironmentVariablesBuilder with the updated environment variables.</returns>
+    [Pure]
+    public IEnvironmentVariablesBuilder Set(IEnumerable<KeyValuePair<string, string>> variables)
+        => SetInternal(variables);
 
     /// <summary>
     /// Sets multiple environment variables from a dictionary.
@@ -104,17 +104,7 @@ public class EnvironmentVariablesBuilder : IEnvironmentVariablesBuilder
     /// <returns>A new instance of the IEnvironmentVariablesBuilder with the updated environment variables.</returns>
     [Pure]
     public IEnvironmentVariablesBuilder Set(IReadOnlyDictionary<string, string> variables)
-    {
-#if NET8_0_OR_GREATER
-        ArgumentNullException.ThrowIfNull(nameof(variables));
-#endif
-        
-        Dictionary<string, string> output = new Dictionary<string, string>(_environmentVariables,
-            StringComparer.Ordinal);
-        output.AddRange(variables);
-
-        return new EnvironmentVariablesBuilder(output);
-    }
+        =>  SetInternal(variables);
 
     /// <summary>
     /// Builds the dictionary of configured environment variables.
