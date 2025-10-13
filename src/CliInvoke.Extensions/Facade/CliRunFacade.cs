15c9c67ad74bc05491938bf942a0ea75fa842185
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-13 15:52:03 +0100

    Update CliRunFacade.cs

--- src/CliInvoke.Extensions/Facade/CliRunFacade.cs
+++ src/CliInvoke.Extensions/Facade/CliRunFacade.cs
@@ -8,8 +8,6 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-using System;
-using System.Collections.Generic;
 using System.IO;
 
 #if NET6_0_OR_GREATER
@@ -20,24 +18,20 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using AlastairLundy.CliInvoke.Core;
-using AlastairLundy.CliInvoke.Core.Builders;
-using AlastairLundy.CliInvoke.Builders;
 using AlastairLundy.CliInvoke.Exceptions;
 
 namespace AlastairLundy.CliInvoke.Extensions;
 
 /// <summary>
-/// A class providing syntactic sugar to make simple uses cases super easy with CliInvoke.
+/// A class providing syntactic sugar to make simple process running uses cases easy with CliInvoke.
 /// </summary>
-public static class Cli
+public static partial class Cli
 {
     /// <summary>
     /// Runs a process asynchronously, waits for exit, and safely disposes of the Process before returning.
     /// </summary>
     /// <param name="processConfigurationInvoker">The process configuration invoker to use to execute the command.</param>
-    /// <param name="targetFilePath">The target file path of the command to be executed.</param>
-    /// <param name="arguments">The arguments to pass to the Command upon execution.</param>
-    /// <param name="workingDirectory">The working directory to be used.</param>
+    /// <param name="processConfiguration"></param>
     /// <param name="exitConfiguration">The exit configuration to use for the process, or the default exit configuration if null.</param>
     /// <param name="cancellationToken">A token to cancel the operation if required.</param>
     /// <returns>The Process Results from running the process.</returns>
@@ -55,20 +49,13 @@ public static class Cli
     [UnsupportedOSPlatform("browser")]
 #endif
     public static async Task<ProcessResult> InvokeAsync(IProcessConfigurationInvoker processConfigurationInvoker,
-        string targetFilePath,
-        IEnumerable<string> arguments, string? workingDirectory = null,
+        ProcessConfiguration processConfiguration,
         ProcessExitConfiguration? exitConfiguration = null,
         CancellationToken cancellationToken = default)
     {
-        IProcessConfigurationBuilder processConfigurationBuilder = new ProcessConfigurationBuilder(targetFilePath)
-            .WithArguments(arguments)
-            .WithWorkingDirectory(workingDirectory ?? Environment.CurrentDirectory);
-
-        ProcessConfiguration configuration = processConfigurationBuilder.Build();
-
         ProcessExitConfiguration processExitConfiguration = exitConfiguration ?? ProcessExitConfiguration.Default;
         
-        return await processConfigurationInvoker.ExecuteAsync(configuration, processExitConfiguration,
+        return await processConfigurationInvoker.ExecuteAsync(processConfiguration, processExitConfiguration,
             cancellationToken: cancellationToken);
     }
 
@@ -77,9 +64,7 @@ public static class Cli
     /// gets Standard Output and Standard Error as Strings, waits for exit, and safely disposes of the Process before returning.
     /// </summary>
     /// <param name="processConfigurationInvoker">The process configuration invoker to use to execute the command.</param>
-    /// <param name="targetFilePath">The target file path of the command to be executed.</param>
-    /// <param name="arguments">The arguments to pass to the Command upon execution.</param>
-    /// <param name="workingDirectory">The working directory to be used.</param>
+    /// <param name="processConfiguration"></param>
     /// <param name="exitConfiguration">The exit configuration to use for the process, or the default exit configuration if null.</param>
     /// <param name="cancellationToken">A token to cancel the operation if required.</param>
     /// <returns>The Buffered Process Results from running the process.</returns>
@@ -97,34 +82,22 @@ public static class Cli
     [UnsupportedOSPlatform("browser")]
 #endif
     public static async Task<BufferedProcessResult> InvokeBufferedAsync(IProcessConfigurationInvoker processConfigurationInvoker,
-        string targetFilePath,
-        IEnumerable<string> arguments, string? workingDirectory = null,
+        ProcessConfiguration processConfiguration,
         ProcessExitConfiguration? exitConfiguration = null,
         CancellationToken cancellationToken = default)
     {
-        IProcessConfigurationBuilder processConfigurationBuilder = new ProcessConfigurationBuilder(targetFilePath)
-            .WithArguments(arguments)
-            .WithWorkingDirectory(workingDirectory ?? Environment.CurrentDirectory)
-            .RedirectStandardOutput(true)
-            .RedirectStandardError(true)
-            .WithWindowCreation(false);
-
-        ProcessConfiguration configuration = processConfigurationBuilder.Build();
-
         ProcessExitConfiguration processExitConfiguration = exitConfiguration ?? ProcessExitConfiguration.Default;
         
-        return await processConfigurationInvoker.ExecuteBufferedAsync(configuration, processExitConfiguration,
+        return await processConfigurationInvoker.ExecuteBufferedAsync(processConfiguration, processExitConfiguration,
             cancellationToken: cancellationToken);
     }
-    
+
     /// <summary>
     /// Runs a process asynchronously with Standard Output and Standard Error Redirection,
     /// gets Standard Output and Standard Error as Strings, waits for exit, and safely disposes of the Process before returning.
     /// </summary>
     /// <param name="processConfigurationInvoker">The process configuration invoker to use to execute the command.</param>
-    /// <param name="targetFilePath">The target file path of the command to be executed.</param>
-    /// <param name="arguments">The arguments to pass to the Command upon execution.</param>
-    /// <param name="workingDirectory">The working directory to be used.</param>
+    /// <param name="processConfiguration"></param>
     /// <param name="exitConfiguration">The exit configuration to use for the process, or the default exit configuration if null.</param>
     /// <param name="cancellationToken">A token to cancel the operation if required.</param>
     /// <returns>The Piped Process Results from running the process.</returns>
@@ -142,23 +115,13 @@ public static class Cli
     [UnsupportedOSPlatform("browser")]
 #endif
     public static async Task<PipedProcessResult> InvokePipedAsync(IProcessConfigurationInvoker processConfigurationInvoker,
-        string targetFilePath,
-        IEnumerable<string> arguments, string? workingDirectory = null,
+        ProcessConfiguration processConfiguration,
         ProcessExitConfiguration? exitConfiguration = null,
         CancellationToken cancellationToken = default)
     {
-        IProcessConfigurationBuilder processConfigurationBuilder = new ProcessConfigurationBuilder(targetFilePath)
-            .WithArguments(arguments)
-            .WithWorkingDirectory(workingDirectory ?? Environment.CurrentDirectory)
-            .RedirectStandardOutput(true)
-            .RedirectStandardError(true)
-            .WithWindowCreation(false);
-
-        ProcessConfiguration configuration = processConfigurationBuilder.Build();
-
         ProcessExitConfiguration processExitConfiguration = exitConfiguration ?? ProcessExitConfiguration.Default;
         
-        return await processConfigurationInvoker.ExecutePipedAsync(configuration, processExitConfiguration,
+        return await processConfigurationInvoker.ExecutePipedAsync(processConfiguration, processExitConfiguration,
             cancellationToken: cancellationToken);
     }
 }
