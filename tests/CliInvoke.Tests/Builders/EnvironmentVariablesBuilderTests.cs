ec681f33306fe5ffab802111c8dd8020b4ccbee6
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-19 15:41:14 +0100

    continue work on Environment Variables Builder Test

--- tests/CliInvoke.Tests/Builders/EnvironmentVariablesBuilderTests.cs
+++ tests/CliInvoke.Tests/Builders/EnvironmentVariablesBuilderTests.cs
@@ -1,10 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using AlastairLundy.CliInvoke.Builders;
+using AlastairLundy.CliInvoke.Core.Builders;
+using Bogus;
 using Xunit;
 
 namespace AlastairLundy.CliInvoke.Tests.Builders;
 
 public class EnvironmentVariablesBuilderTests
 {
+    private readonly Faker _faker = new Faker();
     [Fact]
     public void Constructor_ShouldInstantiate()
     {
@@ -13,14 +20,53 @@ public class EnvironmentVariablesBuilderTests
     }
 
     [Fact]
-    public void Add_ShouldAddVariable()
+    public void Set_SingleVariable_Success()
     {
-        // TODO: Test Add method
+        // Arrange
+        string key = _faker.Database.Column();
+        string value = _faker.Database.Random.Word();
+
+        // Act
+        IEnvironmentVariablesBuilder builder = new EnvironmentVariablesBuilder()
+            .Set(key, value);
+
+        IReadOnlyDictionary<string, string> variables = builder.Build();
+        
+        // Assert
+        Assert.Equal(value,variables[key]);
     }
 
     [Fact]
-    public void Build_ShouldReturnVariables()
+    public void Set_KeyValuePairs_Sequence_Success()
     {
-        // TODO: Test Build method
+        int number = _faker.Random.Int(1, 20);
+        HashSet<string> keys = new HashSet<string>();
+
+        while (keys.Count < number)
+        {
+            keys.Add(_faker.Random.Word().ToLower());
+        }
+        
+        List<string> vals = _faker.Make(number + 50, x =>  _faker.Random.Word())
+            .Distinct(StringComparer.InvariantCultureIgnoreCase)
+            .Take(number)
+            .ToList();
+
+        List<KeyValuePair<string, string>> keyValPairs = (from k in keys
+            from v in vals
+            select new KeyValuePair<string, string>(k, v))
+            .ToList();
+       
+       // Act
+       IEnvironmentVariablesBuilder builder = new EnvironmentVariablesBuilder()
+           .Set(keyValPairs);
+       
+       IReadOnlyDictionary<string, string> variables = builder.Build();
+       
+       // Assert
+       foreach (KeyValuePair<string, string> pair in keyValPairs)
+       {
+           Assert.Equal(pair.Value, variables[pair.Key]);
+       }
     }
 }
