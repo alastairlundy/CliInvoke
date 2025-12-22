f36e497c6b8e3c87f997cef5542c1f41215694f5
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-22 16:24:10 +0000

    Update FilePathResolverTests.cs

--- tests/CliInvoke.Tests/Resolvers/FilePathResolverTests.cs
+++ tests/CliInvoke.Tests/Resolvers/FilePathResolverTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using AlastairLundy.DotPrimitives.IO.Paths;
 using CliInvoke.Core;
 using CliInvoke.Core.Factories;
 using CliInvoke.Factories;
