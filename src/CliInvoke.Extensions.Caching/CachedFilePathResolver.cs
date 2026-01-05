02974d06879d5c42d4a5161c149977282de991c9
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-05 14:28:59 +0000

    add FilePathCacheLifespan

--- src/CliInvoke.Extensions.Caching/CachedFilePathResolver.cs
+++ src/CliInvoke.Extensions.Caching/CachedFilePathResolver.cs
@@ -22,27 +22,33 @@ public class CachedFilePathResolver : FilePathResolver
 {
     private readonly IMemoryCache _cache;
 
+    private const string FilePathCachePrefix = "FilePath_";
     private const string PathExtCacheName = "PathExtData";
     private const string PathCacheName = "PathCacheData";
     
     private TimeSpan PathExtCacheLifespan { get; } = TimeSpan.FromHours(1.0);
-    private TimeSpan PathCacheLifespan { get; } = TimeSpan.FromMinutes(3.0);
+    private TimeSpan PathCacheLifespan { get; } = TimeSpan.FromMinutes(3.0); 
+    
+    private TimeSpan FilePathCacheLifespan { get; } = TimeSpan.FromMinutes(10.0);
 
     /// <summary>
     /// Provides a mechanism for resolving file paths with enhanced performance by using
     /// an in-memory cache. This class extends the functionality of <see cref="FilePathResolver"/>
     /// and allows customizable cache lifespans for PATH contents and PATH environment variable extensions.
     /// </summary>
-    public CachedFilePathResolver(IMemoryCache cache, TimeSpan? defaultPathCacheLifespan = null,
-        TimeSpan? defaultPathExtCacheLifespan = null)
+    public CachedFilePathResolver(IMemoryCache cache, TimeSpan? filePathCacheLifespan = null, TimeSpan? pathCacheLifespan = null,
+        TimeSpan? pathExtCacheLifespan = null)
     {
         _cache = cache;
 
-        if(defaultPathExtCacheLifespan.HasValue)
-            PathExtCacheLifespan = defaultPathExtCacheLifespan.Value;
+        if(filePathCacheLifespan.HasValue)
+            FilePathCacheLifespan = filePathCacheLifespan.Value;
         
-        if(defaultPathCacheLifespan.HasValue)
-            PathCacheLifespan = defaultPathCacheLifespan.Value;
+        if(pathExtCacheLifespan.HasValue)
+            PathExtCacheLifespan = pathExtCacheLifespan.Value;
+        
+        if(pathCacheLifespan.HasValue)
+            PathCacheLifespan = pathCacheLifespan.Value;
     }
 
     /// <summary>
@@ -73,17 +79,32 @@ public class CachedFilePathResolver : FilePathResolver
     public new string ResolveFilePath(string filePathToResolve)
     {
         ArgumentException.ThrowIfNullOrEmpty(filePathToResolve);
-
+        
         if (Path.IsPathRooted(filePathToResolve))
             return filePathToResolve;
+        
+        bool isCached = _cache.TryGetValue($"{FilePathCachePrefix}{filePathToResolve}", out string? path);
+
+        if (isCached && path is not null && File.Exists(path))
+            return path;
         
         bool resolveFromPath =
             ResolveFromPathEnvironmentVariable(filePathToResolve, out FileInfo? filePath);
 
-        if (resolveFromPath && filePath is not null)
-            return filePath.FullName;
+        string output;
 
-        return LocateFileFromDirectory(filePathToResolve).FullName;
+        if (resolveFromPath && filePath is not null)
+        {
+            output = filePath.FullName;
+        }
+        else
+        {
+            output = LocateFileFromDirectory(filePathToResolve).FullName;
+        }
+        
+        _cache.Set($"{FilePathCachePrefix}{filePathToResolve}", output, FilePathCacheLifespan);    
+        
+        return output;
     }
     
     protected new string[] GetPathExtensionsInfo()
--- src/CliInvoke.Extensions.Caching/DependencyInjection/RegisterCachedResolver.cs
+++ src/CliInvoke.Extensions.Caching/DependencyInjection/RegisterCachedResolver.cs
@@ -21,12 +21,13 @@ public static class RegisterCachedResolver
         /// Configures the application to use a cached file path resolver service with a specified lifetime and optional cache lifespans.
         /// </summary>
         /// <param name="serviceLifetime">The <see cref="ServiceLifetime"/> of the cached file path resolver. Defaults to the value of Scoped.</param>
+        /// <param name="filePathCacheLifespan"></param>
         /// <param name="pathCacheLifespan">An optional <see cref="TimeSpan"/> representing the lifespan of the path cache. If null defaults to 3 minutes if not set.</param>
         /// <param name="pathExtensionsLifespan">An optional <see cref="TimeSpan"/> representing the lifespan of the path extension cache. If null, defaults to 1 hour if not set.</param>
         /// <returns>The <see cref="IServiceCollection"/> to allow further configuration chaining.</returns>
         /// <exception cref="NotSupportedException">Thrown when the specified <paramref name="serviceLifetime"/> is not supported.</exception>
         public IServiceCollection UseCachedFilePathResolver(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped,
-            TimeSpan? pathCacheLifespan = null, TimeSpan? pathExtensionsLifespan = null)
+            TimeSpan? filePathCacheLifespan = null, TimeSpan? pathCacheLifespan = null, TimeSpan? pathExtensionsLifespan = null)
         {
             services.AddMemoryCache();
 
@@ -35,17 +36,17 @@ public static class RegisterCachedResolver
                 case ServiceLifetime.Scoped:
                     services.TryAddScoped<IFilePathResolver>(sp => 
                         new CachedFilePathResolver(sp.GetRequiredService<IMemoryCache>(),
-                            pathCacheLifespan, pathExtensionsLifespan));
+                            filePathCacheLifespan, pathCacheLifespan, pathExtensionsLifespan));
                     break;
                 case ServiceLifetime.Singleton:
                     services.TryAddSingleton<IFilePathResolver>(sp => 
                         new CachedFilePathResolver(sp.GetRequiredService<IMemoryCache>(),
-                            pathCacheLifespan, pathExtensionsLifespan));
+                            filePathCacheLifespan, pathCacheLifespan, pathExtensionsLifespan));
                     break;
                 case ServiceLifetime.Transient:
                     services.TryAddTransient<IFilePathResolver>(sp => 
                         new CachedFilePathResolver(sp.GetRequiredService<IMemoryCache>(),
-                            pathCacheLifespan, pathExtensionsLifespan));
+                            filePathCacheLifespan, pathCacheLifespan, pathExtensionsLifespan));
                     break;
                 default:
                     throw new NotSupportedException();
