38f18fa83d30d880cdfb6fbb06a2782239aa47de
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-07 01:26:30 +0000

    Update GracefulCancellation.Windows.cs

--- src/CliInvoke/Helpers/Processes/Cancellation/GracefulCancellation.Windows.cs
+++ src/CliInvoke/Helpers/Processes/Cancellation/GracefulCancellation.Windows.cs
@@ -7,9 +7,6 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
    */
 
-using System.Runtime.InteropServices;
-using System.Threading;
-
 namespace CliInvoke.Helpers.Processes.Cancellation;
 
 internal static partial class GracefulCancellation
