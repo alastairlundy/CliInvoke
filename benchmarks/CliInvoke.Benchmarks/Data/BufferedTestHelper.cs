a0a3bbbf28df1417ae21ea6aa65130109f6be890
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-10 19:29:40 +0000

    add initial mockdata sim tool locating code

--- benchmarks/CliInvoke.Benchmarks/Data/BufferedTestHelper.cs
+++ benchmarks/CliInvoke.Benchmarks/Data/BufferedTestHelper.cs
@@ -1,6 +1,9 @@
 ﻿using AlastairLundy.WhatExecLib;
 using AlastairLundy.WhatExecLib.Detectors;
 using AlastairLundy.WhatExecLib.Locators;
+using CliInvoke.Core;
+using CliInvoke.Factories;
+using CliInvoke.Piping;
 
 namespace CliInvoke.Benchmarking.Data;
 
@@ -8,8 +11,27 @@ public class BufferedTestHelper
 {
     public BufferedTestHelper()
     {
-        TargetFilePath = "";
+        TargetFilePath = ""; 
         
+        TargetFilePath = GetMockDataSimExePath();
+    }
+
+    private string GetMockDataSimExePath()
+    {
+        IProcessInvoker processInvoker = new ProcessInvoker(new FilePathResolver(), new ProcessPipeHandler());
+        
+        ProcessConfigurationFactory processConfigurationFactory = new();
+
+        string whichOrWhere = OperatingSystem.IsWindows() ? "where.exe" : "which";
+        
+        string mockDataExe = OperatingSystem.IsWindows() ? "CliInvokeMockDataSimTool.exe" : "CliInvokeMockDataSimTool";
+        
+        using ProcessConfiguration configuration = processConfigurationFactory.Create(whichOrWhere,
+            mockDataExe);
+        
+        BufferedProcessResult task = processInvoker.ExecuteBufferedAsync(configuration).Result;
+
+        return task.StandardOutput.Split(Environment.NewLine).First();
     }
 
     public string TargetFilePath
