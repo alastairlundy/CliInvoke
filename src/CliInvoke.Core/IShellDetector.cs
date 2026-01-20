eec734cb1afa79202e3f0255d9407e76df27f35a
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-20 16:10:19 +0000

    add Shell Detector interface code

---
+++ src/CliInvoke.Core/IShellDetector.cs
@@ -0,0 +1,23 @@
+﻿/*
+    CliInvoke.Core
+    Copyright (C) 2024-2026  Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+namespace CliInvoke.Core;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IShellDetector
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<ShellInformation> ResolveDefaultShellAsync(CancellationToken cancellationToken);
+}
