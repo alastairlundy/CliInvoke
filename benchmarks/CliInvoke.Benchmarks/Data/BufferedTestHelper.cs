67cc4349d1c58df818cd843a0266f1804ad25a02
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-21 14:31:40 +0000

    fix locating mock data sim tool

--- benchmarks/CliInvoke.Benchmarks/Data/BufferedTestHelper.cs
+++ benchmarks/CliInvoke.Benchmarks/Data/BufferedTestHelper.cs
@@ -2,26 +2,53 @@
 using AlastairLundy.WhatExecLib.Detectors;
 using AlastairLundy.WhatExecLib.Locators;
 using CliInvoke.Core;
+using CliInvoke.Core.Builders;
 using CliInvoke.Factories;
 using CliInvoke.Piping;
+using DotExtensions.IO.Directories;
 
 namespace CliInvoke.Benchmarking.Data;
 
 public class BufferedTestHelper
 {
+    private DotnetCommandHelper _dotnetCommandHelper;
+    
     public BufferedTestHelper()
     {
         TargetFilePath = ""; 
         
         TargetFilePath = GetMockDataSimExePath();
+        _dotnetCommandHelper = new DotnetCommandHelper();
     }
 
     private string GetMockDataSimExePath()
     {
         IProcessInvoker processInvoker = new ProcessInvoker(new FilePathResolver(), new ProcessPipeHandler());
+
+        DirectoryInfo directoryInfo = new DirectoryInfo(Environment.CurrentDirectory)
+            .Root;
+
+        FileInfo? mockDataProjectFile = directoryInfo.SafelyEnumerateFiles("CliInvoke.Benchmarking.MockDataSimulationTool.csproj",
+                SearchOption.AllDirectories)
+            .FirstOrDefault();
         
         ProcessConfigurationFactory processConfigurationFactory = new();
 
+        if (mockDataProjectFile is not null)
+        {
+            using ProcessConfiguration buildConfig =
+                new ProcessConfiguration(OperatingSystem.IsWindows() ? "dotnet.exe" : "dotnet",
+                    false, true,
+                    true,
+                    "build -c Release", mockDataProjectFile.Directory?.FullName);
+
+            Task<BufferedProcessResult> resultTask = processInvoker.ExecuteBufferedAsync(buildConfig, 
+                ProcessExitConfiguration.DefaultNoException,
+                false, CancellationToken.None);
+
+            resultTask.Wait();
+        }
+        
         string whichOrWhere = OperatingSystem.IsWindows() ? "where.exe" : "which";
         
         string mockDataExe = OperatingSystem.IsWindows() ? "CliInvokeMockDataSimTool.exe" : "CliInvokeMockDataSimTool";
@@ -29,9 +56,16 @@ public class BufferedTestHelper
         using ProcessConfiguration configuration = processConfigurationFactory.Create(whichOrWhere,
             mockDataExe);
         
-        BufferedProcessResult task = processInvoker.ExecuteBufferedAsync(configuration).Result;
+        Task<BufferedProcessResult> task = processInvoker.ExecuteBufferedAsync(configuration, ProcessExitConfiguration.DefaultNoException,
+            false, CancellationToken.None);
+        task.Wait(0);
+        
+        if (task.Result.ExitCode != 0)
+        {
+            throw new ArgumentException("CliInvoke Mock Data Simulation Tool could not be found.");
+        }
 
-        return task.StandardOutput.Split(Environment.NewLine).First();
+        return task.Result.StandardOutput.Split(Environment.NewLine).First();
     }
 
     public string TargetFilePath
