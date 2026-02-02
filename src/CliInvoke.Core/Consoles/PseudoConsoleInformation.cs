e44b3083405a1266012ff488678e39a97a0d03b4
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-02 11:49:31 +0000

    Create PseudoConsoleInformation.cs

---
+++ src/CliInvoke.Core/Consoles/PseudoConsoleInformation.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace CliInvoke.Core.Consoles;
+
+public class PseudoConsoleInformation
+{
+    /// <summary>
+    /// The 
+    /// </summary>
+    public int Columns { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int Rows { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool Enabled { get; set; }
+
+    public PseudoConsoleInformation()
+    {
+        
+    }
+}
