cae379f04c138386fc65b572e12081899a25998a
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-24 00:31:51 +0000

    add fuzz testing

---
+++ src/CliInvoke.Tests.Fuzzing/Program.cs
@@ -0,0 +1,37 @@
+﻿using SharpFuzz;
+using CliInvoke.Builders;
+using CliInvoke.Core.Builders;
+
+Fuzzer.LibFuzzer.Run(stream =>
+{
+    string input = stream.ToString();
+
+    if (string.IsNullOrEmpty(input))
+        return;
+
+    try
+    {
+        IArgumentsBuilder builder = new ArgumentsBuilder();
+                
+        // Fuzzing basic Add and EscapeCharacters
+        builder = builder.Add(input, true);
+       
+        // Fuzzing Enumerable Add
+        builder = builder.AddEnumerable([input, input], true);
+        
+        // Fuzzing Clear
+        builder.Clear();
+
+        // Fuzzing ProcessConfigurationBuilder
+        if (!string.IsNullOrWhiteSpace(input))
+        {
+            IProcessConfigurationBuilder processConfigurationBuilder = new ProcessConfigurationBuilder(input);
+            processConfigurationBuilder = processConfigurationBuilder.SetArguments(input)
+                .SetWorkingDirectory(input);
+            processConfigurationBuilder.Build();
+        }
+    }
+    catch (ArgumentException) { }
+    catch (InvalidOperationException) { }
+    catch (NullReferenceException) { }
+});
