e26d2710deb6bb20c1bec27cc83cea9e0570ac92
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-25 18:31:03 +0000

    Reapply "create external process factory"
    This reverts commit de058599202344e875b3eff88a72e2b82410a265.

---
+++ src/CliInvoke.Core/Factories/IExternalProcessFactory.cs
@@ -0,0 +1,34 @@
+﻿/*
+    CliInvoke.Core
+    Copyright (C) 2024-2026  Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+using CliInvoke.Core.Processes;
+
+namespace CliInvoke.Core.Factories;
+
+/// <summary>
+/// Represents an interface for creating instances of the <see cref="IExternalProcess"/> interface.
+/// </summary>
+public interface IExternalProcessFactory
+{
+    /// <summary>
+    /// Creates an instance of the <see cref="IExternalProcess"/> interface based on the provided process configuration.
+    /// </summary>
+    /// <param name="configuration">The configuration details for creating the external process.</param>
+    /// <returns>An instance of <see cref="IExternalProcess"/> configured according to the specified <paramref name="configuration"/>.</returns>
+    IExternalProcess CreateExternalProcess(ProcessConfiguration configuration);
+
+    /// <summary>
+    /// Creates an instance of the <see cref="IExternalProcess"/> interface based on the provided process configuration.
+    /// </summary>
+    /// <param name="configuration">The configuration details for creating the external process.</param>
+    /// <param name="exitConfiguration">The process exit configuration details for configuring process exit behaviour.</param>
+    /// <returns>An instance of <see cref="IExternalProcess"/> configured according to the specified <paramref name="configuration"/>.</returns>
+    IExternalProcess CreateExternalProcess(ProcessConfiguration configuration,
+        ProcessExitConfiguration exitConfiguration);
+}
