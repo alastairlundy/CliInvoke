bd1ca48b6abcc48d863acc27a29cdf9917be9fa0
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-04-01 17:24:07 +0100

    Update ForcefulCancellation.cs

--- src/CliInvoke/Helpers/Processes/Cancellation/ForcefulCancellation.cs
+++ src/CliInvoke/Helpers/Processes/Cancellation/ForcefulCancellation.cs
@@ -7,8 +7,6 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
    */
 
-using DotExtensions.Dates;
-
 namespace CliInvoke.Helpers.Processes.Cancellation;
 
 internal static class ForcefulCancellation
