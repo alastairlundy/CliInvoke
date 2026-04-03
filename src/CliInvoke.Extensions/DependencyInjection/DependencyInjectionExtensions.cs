4839dff8e12173dc4cb15404ee836b411677fc1f
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-04-03 13:16:03 +0100

    fix DI issues

--- src/CliInvoke.Extensions/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/CliInvoke.Extensions/DependencyInjection/DependencyInjectionExtensions.cs
@@ -12,7 +12,9 @@ using CliInvoke.Extensibility;
 using CliInvoke.Factories;
 using CliInvoke.Validation;
 
+using WhatExec.Lib.Abstractions.Detectors;
 using WhatExec.Lib.Abstractions.Resolvers;
+using WhatExec.Lib.Detectors;
 using WhatExec.Lib.Resolvers;
 
 // ReSharper disable RedundantAssignment
@@ -36,6 +38,9 @@ public static partial class DependencyInjectionExtensions
         switch (lifetime)
         {
             case ServiceLifetime.Singleton:
+                services.TryAddSingleton<IExecutableFileDetector, ExecutableFileDetector>();
+                services.TryAddSingleton<IPathEnvironmentVariableDetector, PathEnvironmentVariableDetector>();
+                services.TryAddSingleton<IPathEnvironmentVariableResolver, PathEnvironmentVariableResolver>();
                 services.TryAddSingleton<IExecutableFileResolver, ExecutableFileResolver>();
 
                 services.TryAddSingleton<IProcessResultValidator<ProcessResult>>(_ =>
@@ -56,6 +61,9 @@ public static partial class DependencyInjectionExtensions
                 services.AddSingleton<IShellDetector, ShellDetector>();
                 break;
             case ServiceLifetime.Scoped:
+                services.TryAddScoped<IExecutableFileDetector, ExecutableFileDetector>();
+                services.TryAddScoped<IPathEnvironmentVariableDetector, PathEnvironmentVariableDetector>();
+                services.TryAddScoped<IPathEnvironmentVariableResolver, PathEnvironmentVariableResolver>();
                 services.TryAddScoped<IExecutableFileResolver, ExecutableFileResolver>();
 
                 services.TryAddScoped<IProcessResultValidator<ProcessResult>>(_ =>
@@ -76,6 +84,9 @@ public static partial class DependencyInjectionExtensions
                 services.AddScoped<IShellDetector, ShellDetector>();
                 break;
             case ServiceLifetime.Transient:
+                services.TryAddTransient<IExecutableFileDetector, ExecutableFileDetector>();
+                services.TryAddTransient<IPathEnvironmentVariableDetector, PathEnvironmentVariableDetector>();
+                services.TryAddTransient<IPathEnvironmentVariableResolver, PathEnvironmentVariableResolver>();
                 services.TryAddTransient<IExecutableFileResolver, ExecutableFileResolver>();
 
                 services.TryAddTransient<IProcessResultValidator<ProcessResult>>(_ =>
