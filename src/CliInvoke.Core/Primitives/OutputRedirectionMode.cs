c7192b7b74eedb2867a9fb03f3a56f35875adc50
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-04-07 18:40:03 +0100

    add missing license notices

--- src/CliInvoke.Core/Primitives/OutputRedirectionMode.cs
+++ src/CliInvoke.Core/Primitives/OutputRedirectionMode.cs
@@ -1,5 +1,17 @@
-﻿namespace CliInvoke.Core;
+﻿/*
+    CliInvoke
+    Copyright (C) 2024-2026  Alastair Lundy
 
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+namespace CliInvoke.Core;
+
+/// <summary>
+/// Defines the modes for redirecting process output.
+/// </summary>
 public enum OutputRedirectionMode
 {
     /// <summary>
