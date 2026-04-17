d89165c3a7bd09a34610fe4e99541d59e65919cb
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-04-17 09:23:52 +0100

    add some xml doc comments

--- src/CliInvoke/Extensions/CliRun.cs
+++ src/CliInvoke/Extensions/CliRun.cs
@@ -10,17 +10,21 @@
 namespace CliInvoke;
 
 /// <summary>
-/// 
+/// Provides static beginner-friendly methods for executing command-line processes
+/// with various configurations and behaviours.
 /// </summary>
 public static class CliRun
 {
     private static Func<IProcessInvoker> _processInvokerFactory = () => new 
         ProcessInvoker(FilePathResolver.Shared);
-    
+
     /// <summary>
-    /// 
+    /// Configures the process invoker to be used for executing command-line processes.
     /// </summary>
-    /// <param name="processInvoker"></param>
+    /// <param name="processInvoker">
+    /// An implementation of the <see cref="IProcessInvoker"/> interface, which defines the logic for creating
+    /// and managing process executions. This parameter allows customization of process invocation behaviour.
+    /// </param>
     public static void UseProcessInvoker(IProcessInvoker processInvoker)
     {
         _processInvokerFactory = () => processInvoker;
@@ -30,20 +34,33 @@ public static class CliRun
         => _processInvokerFactory.Invoke();
 
     /// <summary>
-    /// 
+    /// Executes a specified process with the provided parameters asynchronously and returns the resulting process data.
     /// </summary>
-    /// <param name="targetFilePath"></param>
-    /// <param name="arguments"></param>
-    /// <param name="workingDirectory"></param>
-    /// <param name="timeoutTimeSpan"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <param name="targetFilePath">
+    /// The path of the executable file to be run.
+    /// </param>
+    /// <param name="arguments">
+    /// Command-line arguments for the executable. Defaults to an empty string if not specified.
+    /// </param>
+    /// <param name="workingDirectory">
+    /// The directory in which the process will be executed. If null, the current directory is used.
+    /// </param>
+    /// <param name="timeoutTimeSpan">
+    /// The maximum duration that the process is allowed to run before it times out. If null, a default value is applied.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token that can signal the cancellation of the operation before its completion.
+    /// </param>
+    /// <returns>
+    /// A task representing the asynchronous operation. The task result is a <see cref="ProcessResult"/> object
+    /// containing details of the executed process, including exit status and runtime information.
+    /// </returns>
     public static async Task<ProcessResult> RunAsync(string targetFilePath,
         string arguments = "", string? workingDirectory = null, TimeSpan? timeoutTimeSpan = null,
         CancellationToken cancellationToken = default)
     {
         workingDirectory ??= Environment.CurrentDirectory;
-        
+
         using ProcessConfiguration configuration = ProcessConfiguration.Create(targetFilePath,
             arguments, workingDirectory, false);
         
@@ -71,16 +88,29 @@ public static class CliRun
         return await GetInvoker().ExecuteAsync(configuration, 
             exitConfiguration, cancellationToken);
     }
-    
+
     /// <summary>
-    /// 
+    /// Executes a specified process asynchronously with the provided parameters and returns the buffered process result.
     /// </summary>
-    /// <param name="targetFilePath"></param>
-    /// <param name="arguments"></param>
-    /// <param name="workingDirectory"></param>
-    /// <param name="timeoutTimeSpan"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <param name="targetFilePath">
+    /// The path of the executable file to run.
+    /// </param>
+    /// <param name="arguments">
+    /// Command-line arguments to pass to the executable. Defaults to an empty string if not specified.
+    /// </param>
+    /// <param name="workingDirectory">
+    /// The working directory for the process. If null, the current directory is used.
+    /// </param>
+    /// <param name="timeoutTimeSpan">
+    /// The maximum duration that the process can run before timing out. If null, a default timeout is applied.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to monitor for cancellation requests, allowing the operation to be cancelled before it completes.
+    /// </param>
+    /// <returns>
+    /// A task representing the asynchronous operation. The result is a <see cref="BufferedProcessResult"/>
+    /// object containing the full output of the process and execution details.
+    /// </returns>
     public static async Task<BufferedProcessResult> RunBufferedAsync(string targetFilePath,
         string arguments = "", string? workingDirectory = null, TimeSpan? timeoutTimeSpan = null,
         CancellationToken cancellationToken = default)
@@ -97,7 +127,8 @@ public static class CliRun
         
         return await GetInvoker().ExecuteBufferedAsync(configuration, exitConfiguration, cancellationToken);
     }
-        
+
+
     /// <summary>
     /// 
     /// </summary>
@@ -116,14 +147,27 @@ public static class CliRun
     }
 
     /// <summary>
-    /// 
+    /// Executes a process with the specified parameters and returns a result containing the process's piped data and exit information.
     /// </summary>
-    /// <param name="targetFilePath"></param>
-    /// <param name="arguments"></param>
-    /// <param name="workingDirectory"></param>
-    /// <param name="timeoutTimeSpan"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <param name="targetFilePath">
+    /// The file path of the target executable to be run.
+    /// </param>
+    /// <param name="arguments">
+    /// The command-line arguments to pass to the executable. Defaults to an empty string if not specified.
+    /// </param>
+    /// <param name="workingDirectory">
+    /// The working directory in which the process will run. If null, the current directory is used.
+    /// </param>
+    /// <param name="timeoutTimeSpan">
+    /// The maximum allowed duration for the process to complete before timing out. If null, a default timeout is applied.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to monitor for cancellation requests, allowing the operation to be cancelled before completion.
+    /// </param>
+    /// <returns>
+    /// A task representing the asynchronous operation. The task result contains a <see cref="PipedProcessResult"/> object
+    /// with details about the executed process, including piped output and exit status.
+    /// </returns>
     public static async Task<PipedProcessResult> RunPipedAsync(string targetFilePath,
         string arguments = "", string? workingDirectory = null, TimeSpan? timeoutTimeSpan = null,
         CancellationToken cancellationToken = default)
@@ -140,14 +184,23 @@ public static class CliRun
         
         return await GetInvoker().ExecutePipedAsync(configuration, exitConfiguration, cancellationToken);
     }
-    
+
     /// <summary>
-    /// 
+    /// Executes a process using the specified configuration and returns a result containing piped process data.
     /// </summary>
-    /// <param name="configuration"></param>
-    /// <param name="exitConfiguration"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <param name="configuration">
+    /// The configuration for the process to be executed, including details such as file path, arguments, and environment settings.
+    /// </param>
+    /// <param name="exitConfiguration">
+    /// An optional configuration for managing the process exit behaviour. If null, a default graceful exit configuration is used.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to monitor for cancellation requests, allowing the operation to be cancelled before completion.
+    /// </param>
+    /// <returns>
+    /// A task representing the asynchronous operation. The task result contains a <see cref="PipedProcessResult"/> object
+    /// with details about the executed process, including piped output and exit information.
+    /// </returns>
     public static async Task<PipedProcessResult> RunPipedAsync(
         ProcessConfiguration configuration,
         ProcessExitConfiguration? exitConfiguration = null,
