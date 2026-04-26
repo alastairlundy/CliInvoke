74055a488fa9457be870680a4c9dca1cecbd66e1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-04-26 16:28:57 +0100

    spelling issues

--- src/CliInvoke.Specializations/Invokers/PowershellProcessInvoker.cs
+++ src/CliInvoke.Specializations/Invokers/PowershellProcessInvoker.cs
@@ -12,6 +12,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using CliInvoke.Specializations.Configurations;
+#pragma warning disable CS0618 // Type or member is obsolete
 
 namespace CliInvoke.Specializations;
 
@@ -20,7 +21,7 @@ namespace CliInvoke.Specializations;
 /// methods to run processes in buffered, piped, or standard modes.
 /// </summary>
 /// <remarks>
-/// The <c>PowershellProcessInvoker</c> class specializes in executing commands via PowerShell, utilizing the
+/// The <c>PowershellProcessInvoker</c> class specialises in executing commands via PowerShell, utilising the
 /// underlying process invoker functionality. It is designed for scenarios where PowerShell-specific process
 /// handling and configurations are required, such as redirecting outputs or managing window creation.
 /// </remarks>
@@ -54,7 +55,7 @@ public class PowershellProcessInvoker : RunnerProcessInvokerBase
     /// Executes a PowerShell process asynchronously using the specified configuration.
     /// </summary>
     /// <param name="processConfiguration">The configuration of the process to execute.</param>
-    /// <param name="processExitConfiguration">Optional configuration for handling the process exit behavior. Defaults to null.</param>
+    /// <param name="processExitConfiguration">Optional configuration for handling the process exit behaviour. Defaults to null.</param>
     /// <param name="disposeOfConfig">Specifies whether to dispose of the configuration after execution. Defaults to true.</param>
     /// <param name="cancellationToken">A token to cancel the asynchronous operation. Defaults to CancellationToken.None.</param>
     /// <returns>A task that represents the asynchronous operation. The task result contains a <see cref="ProcessResult"/> object with the details of the process execution outcome.</returns>
@@ -79,7 +80,7 @@ public class PowershellProcessInvoker : RunnerProcessInvokerBase
     /// Executes a PowerShell process asynchronously with buffered input and output.
     /// </summary>
     /// <param name="processConfiguration">The configuration of the process to execute.</param>
-    /// <param name="processExitConfiguration">Optional configuration for handling the process exit behavior. Defaults to null.</param>
+    /// <param name="processExitConfiguration">Optional configuration for handling the process exit behaviour. Defaults to null.</param>
     /// <param name="disposeOfConfig">Specifies whether to dispose of the configuration after execution. Defaults to true.</param>
     /// <param name="cancellationToken">A token to cancel the asynchronous operation. Defaults to CancellationToken.None.</param>
     /// <returns>A task that represents the asynchronous operation. The task result contains a <see cref="BufferedProcessResult"/> object with the details of the process execution outcome.</returns>
@@ -105,7 +106,7 @@ public class PowershellProcessInvoker : RunnerProcessInvokerBase
     /// Executes a PowerShell process asynchronously with piped input and output.
     /// </summary>
     /// <param name="processConfiguration">The configuration of the process to execute.</param>
-    /// <param name="processExitConfiguration">Optional configuration for handling the process exit behavior. Defaults to null.</param>
+    /// <param name="processExitConfiguration">Optional configuration for handling the process exit behaviour. Defaults to null.</param>
     /// <param name="disposeOfConfig">Specifies whether to dispose of the configuration after execution. Defaults to true.</param>
     /// <param name="cancellationToken">A token to cancel the asynchronous operation. Defaults to CancellationToken.None.</param>
     /// <returns>A task that represents the asynchronous operation. The task result contains a <see cref="PipedProcessResult"/> object with the details of the process execution outcome.</returns>
