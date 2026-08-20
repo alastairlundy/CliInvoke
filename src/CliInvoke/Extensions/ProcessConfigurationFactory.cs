fe37e8d69fcb0518e5d54083bb01aa1fdfe8453f
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-08-21 00:51:25 +0100

    Update ProcessConfigurationFactory.cs

--- src/CliInvoke/Extensions/ProcessConfigurationFactory.cs
+++ src/CliInvoke/Extensions/ProcessConfigurationFactory.cs
@@ -7,8 +7,9 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
    */
 
-using System.Linq;
 using System.Text;
+
+using CliInvoke.Builders;
 using CliInvoke.Core.Configuration;
 
 namespace CliInvoke;
