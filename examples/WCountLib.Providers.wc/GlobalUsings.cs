9a48ad5a2b666ccf22bbe36ccaa825d5cf36eeb2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-08-25 15:33:30 +0100

    Update GlobalUsings.cs

--- examples/WCountLib.Providers.wc/GlobalUsings.cs
+++ examples/WCountLib.Providers.wc/GlobalUsings.cs
@@ -1,12 +1,10 @@
 // Global using directives
 
 global using System;
-global using System.Collections.Generic;
 global using System.IO;
 global using System.Runtime.Versioning;
 global using System.Text;
 global using System.Threading.Tasks;
 global using CliInvoke.Core;
-global using CliInvoke.Core.Factories;
 global using WCountLib.Providers.wc.Abstractions.Counters;
 global using WCountLib.Providers.wc.Helpers;
